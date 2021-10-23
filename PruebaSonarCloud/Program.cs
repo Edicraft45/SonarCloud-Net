cb8cfeb3ca95988362f64ae60728aad687ce4d50
edi uriel Maldonado Hernández <edi_uriel2@hotmail.com>
2021-10-22 22:54:51 -0700

    version 1

---
+++ PruebaSonarCloud/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebaSonarCloud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba de SonarCloud");
+        }
+    }
+}
